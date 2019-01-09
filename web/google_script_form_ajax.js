4a96ec96eb95d7f655519cee15c243e9e8320eb6
Julian92UCLM <33147643+Julian92UCLM@users.noreply.github.com>
2019-01-09 11:23:28 +0100

    con enlace

--- web/google_script_form_ajax.js
+++ web/google_script_form_ajax.js
@@ -1,5 +1,5 @@
 var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec'
+    url = 'https://script.google.com/macros/s/AKfycbx-_-p71n32B8ctqdrFp9r4CGOweUqv7cWT0DXzLoBK0zP5a5A/exec'
 
 $('#submit-form').on('click', function(e) {
   e.preventDefault();
